9ea5399cc3f34ad640aaea7d383c3351f695a2f9
goffinet <goffinet@goffinet.org>
2021-11-14 16:11:50 +0100

    Create setup-controller.sh

---
+++ tests/setup-controller.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+hostnamectl set-hostname controller
+yum -y install python3-pip git sshpass python3-paramiko python3-netaddr python3-ansible-lint ansible git dnsmasq
+cat << EOF > /etc/dnsmasq.conf
+interface=lo0
+interface=eth0
+dhcp-range=11.12.13.100,11.12.13.150,255.255.255.0,512h
+dhcp-option=3
+EOF
+cat << EOF > /etc/sysconfig/network-scripts/ifcfg-eth0
+DEVICE=eth0
+BOOTPROTO=none
+ONBOOT=yes
+TYPE=Ethernet
+IPADDR=11.12.13.1
+PREFIX=24
+IPV4_FAILURE_FATAL=no
+DNS1=127.0.0.1
+EOF
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+rm -f /etc/resolv.conf
+echo "nameserver 127.0.0.1" > /etc/resolv.conf
+echo "nameserver 1.1.1.1" >> /etc/resolv.conf
+chattr +i /etc/resolv.conf
+systemctl enable dnsmasq
+sed -i 's/^#\$ModLoad imudp/$ModLoad imudp/g' /etc/rsyslog.conf
+sed -i 's/^#\$UDPServerRun 514/$UDPServerRun 514/g' /etc/rsyslog.conf
+sed -i 's/^#\$ModLoad imtcp/$ModLoad imtcp/g' /etc/rsyslog.conf
+sed -i 's/^#\$InputTCPServerRun 514/$InputTCPServerRun 514/g' /etc/rsyslog.conf
+systemctl restart rsyslog
+firewall-cmd --permanent --add-service dhcp
+firewall-cmd --permanent --add-service dns
+firewall-cmd --permanent --add-service syslog
+firewall-cmd --reload
+git clone https://github.com/goffinet/ansible-ccna-lab
