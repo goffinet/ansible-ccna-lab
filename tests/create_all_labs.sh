f616d1c49bb7e71fbbc88fb78941273fd200638d
goffinet <goffinet@goffinet.org>
2021-11-21 18:48:35 +0100

    Create create_all_labs.sh

---
+++ tests/create_all_labs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ans="ansible-playbook lab_setup.yml -t provision -i"
+
+$ans inventories/bipod/hosts
+$ans inventories/ccna/hosts
+$ans inventories/ccnp/hosts
+$ans inventories/custom/ccna_remote/hosts
+$ans inventories/custom/etherchannel/hosts
+$ans inventories/custom/ospf_multiarea/hosts
+$ans inventories/custom/ospf_neighbors/hosts
+$ans inventories/custom/osseclab/hosts
+$ans inventories/custom/startup_ios/hosts
+$ans inventories/custom/startup_linux/hosts
+$ans inventories/custom/tripod_l2/hosts
+$ans inventories/gateway/hosts
+$ans inventories/networking_workshop/hosts
+$ans inventories/router_on_a_stick/hosts
+$ans inventories/switchblock/hosts
+$ans inventories/tripod/hosts
